bc75103e6ed3c12e7c3305461dfb602e3187995d
Ana1ogr1z <sipilovyar18@gmail.com>
2025-09-28 12:24:21 +0300

    Config.php

--- Cfg/Config.php
+++ Cfg/Config.php
@@ -1,15 +1,1 @@
-<?php
 
-    namespace Cfg;
-
-    abstract class Config
-    {
-        /**
-         * @var string
-         */
-        protected $server = "localhost";
-        protected $user = "root";
-        protected $db_password = "19022007QaZ";
-        protected $db_name = "smartsoft";
-    }
-    ?>
